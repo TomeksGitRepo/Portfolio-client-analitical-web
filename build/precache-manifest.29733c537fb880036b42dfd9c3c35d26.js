ee796827fa72798efbd1ae16e17ba862abdf1ed0
ServerPortfolio <ServerPortfolio@portfolio.pl>
2021-10-15 13:04:06 +0200

    changed url to working app

---
+++ build/precache-manifest.29733c537fb880036b42dfd9c3c35d26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43810f20216ed0d8ca65b4db30937b55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24a891d1774e9ee1a131",
+    "url": "/static/css/main.5976ee73.chunk.css"
+  },
+  {
+    "revision": "8b8db3584046af9afe86",
+    "url": "/static/js/2.8c00b3ad.chunk.js"
+  },
+  {
+    "revision": "24a891d1774e9ee1a131",
+    "url": "/static/js/main.e1565ae7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3be95e8a674d07af82e831edd1b21b08",
+    "url": "/static/media/stopa4-02.3be95e8a.png"
+  }
+]);
